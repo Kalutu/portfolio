6d688d3621be0a1cfb20ee318d579ca6db80671c
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-11-19 15:24:45 +0300

    Revert "Made about responsive"
    This reverts commit 8b7a0f9a58a1330941a798527cb826b15b8c1ea5.

--- src/Components/About.js
+++ src/Components/About.js
@@ -22,16 +22,16 @@ export default function Intro(){
 
                         <a className="btn btn-primary py-3 px-5 mt-3" href="">Read More</a>
                     </div>
-                    <div className="col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
-                                <img className="img-fluid rounded" src="img/about-1.jpeg" alt=""/>
-                    </div>
-                    {/* <div className="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
-                        <div className="row g-3 mb-4">
+                    <div className="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
+                        <div className="row g-3 mb-4 position-relative">
+                            <div className="col-sm-6">
+                                <img className="img-fluid rounded" src="img/about-1.jpeg" alt="" style={{marginBottom:"75%"}}/>
+                            </div>
                             <div className="col-sm-6 position-absolute" style={{top:"45%", left:"35%"}}>
                                 <img className="img-fluid rounded" src="img/about-2.jpg" alt=""/>
                             </div>
                         </div>
-                        <div className="d-flex align-items-center mb-3">
+                        {/* <div className="d-flex align-items-center mb-3">
                             <h5 className="border-end pe-3 me-3 mb-0">Happy Clients</h5>
                             <h2 className="text-primary fw-bold mb-0" data-toggle="counter-up">1234</h2>
                         </div>
@@ -39,8 +39,8 @@ export default function Intro(){
                         <div className="d-flex align-items-center mb-3">
                             <h5 className="border-end pe-3 me-3 mb-0">Projects Completed</h5>
                             <h2 className="text-primary fw-bold mb-0" data-toggle="counter-up">1234</h2>
-                        </div>
-                    </div> */}
+                        </div> */}
+                    </div>
                 </div>
             </div>
         </div>
